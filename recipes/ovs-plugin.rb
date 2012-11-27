336f701a6202eb072a6668359bddba9d2b0a3742
Jason Cannavale <jason.cannavale@rackspace.com>
2012-11-27 13:50:02 -0600

    fixing up ovs-plugin

--- recipes/ovs-plugin.rb
+++ recipes/ovs-plugin.rb
@@ -46,7 +46,55 @@ service "openvswitch-switch" do
 end
 
 mysql_info = get_access_endpoint("mysql-master", "mysql", "db")
+ks_admin_endpoint = get_access_endpoint("keystone", "keystone", "admin-api")
+rabbit_info = get_access_endpoint("rabbitmq-server", "rabbitmq", "queue")
+api_endpoint = get_bind_endpoint("quantum", "api")
 local_ip = get_ip_for_net('nova', node)		### FIXME
+
+template "/etc/quantum/api-paste.ini" do
+    source "#{release}/api-paste.ini.erb"
+    owner "root"
+    group "root"
+    mode "0644"
+    variables(
+        "keystone_api_ipaddress" => ks_admin_endpoint["host"],
+        "keystone_admin_port" => ks_admin_endpoint["port"],
+        "keystone_protocol" => ks_admin_endpoint["scheme"],
+        "service_tenant_name" => node["quantum"]["service_tenant_name"],
+        "service_user" => node["quantum"]["service_user"],
+        "service_pass" => node["quantum"]["service_pass"]
+    )
+end
+
+template "/etc/quantum/quantum.conf" do
+    source "#{release}/quantum.conf.erb"
+    owner "root"
+    group "root"
+    mode "0644"
+    variables(
+        "quantum_debug" => node["quantum"]["debug"],
+        "quantum_verbose" => node["quantum"]["verbose"],
+        "quantum_ipaddress" => api_endpoint["host"],
+        "quantum_port" => api_endpoint["port"],
+        "rabbit_ipaddress" => rabbit_info["host"],
+        "rabbit_port" => rabbit_info["port"],
+        "overlapping_ips" => node["quantum"]["overlap_ips"],
+        "quantum_plugin" => node["quantum"]["plugin"]
+    )
+end
+
+# execute "ovs-vsctl add-br br-int" do
+    # command "ovs-vsctl add-br #{node["quantum"]["ovs"]["integration_bridge"]}"
+    # action :run
+    # not_if "ovs-vsctl show | grep 'Bridge br-int'" ## FIXME
+# end
+
+# execute "ovs-vsctl add-br br-tun" do
+    # command "ovs-vsctl add-br #{node["quantum"]["ovs"]["tunnel_bridge"]}"
+    # action :run
+    # only_if { node["quantum"]["ovs"]["tunneling"] == "True" }
+    # not_if "ovs-vsctl show | grep 'Bridge br-tun'" ## FIXME
+# end
 
 template "/etc/quantum/plugins/openvswitch/ovs_quantum_plugin.ini" do
     source "#{release}/ovs_quantum_plugin.ini.erb"
@@ -63,23 +111,13 @@ template "/etc/quantum/plugins/openvswitch/ovs_quantum_plugin.ini" do
 	    "ovs_tunnel_ranges" => node["quantum"]["ovs"]["tunnel_ranges"],
 	    "ovs_integration_bridge" => node["quantum"]["ovs"]["integration_bridge"],
 	    "ovs_tunnel_bridge" => node["quantum"]["ovs"]["tunnel_bridge"],
+	    "ovs_debug" => node["quantum"]["debug"],
+	    "ovs_verbose" => node["quantum"]["verbose"],
 	    "ovs_local_ip" => local_ip
     )
-    notifies :restart, resources(:service => "quantum-server"), :immediately
+    # notifies :restart, resources(:service => "quantum-server"), :immediately
     notifies :restart, resources(:service => "quantum-plugin-openvswitch-agent"), :immediately
     notifies :enable, resources(:service => "quantum-plugin-openvswitch-agent"), :immediately
     notifies :restart, resources(:service => "openvswitch-switch"), :immediately
 end
 
-execute "ovs-vsctl add-br br-int" do
-    command "ovs-vsctl add-br #{node["quantum"]["ovs"]["integration_bridge"]}"
-    action :run
-    not_if "ovs-vsctl show | grep 'Bridge br-int'" ## FIXME
-end
-
-execute "ovs-vsctl add-br br-tun" do
-    command "ovs-vsctl add-br #{node["quantum"]["ovs"]["tunnel_bridge"]}"
-    action :run
-    only_if { node["quantum"]["ovs"]["tunneling"] == "True" }
-    not_if "ovs-vsctl show | grep 'Bridge br-tun'" ## FIXME
-end
